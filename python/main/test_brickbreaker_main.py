a3315bbbdcb678f758cefbfb7c3fb72787037417
L3oN <leon_o@gmx.de>
2023-02-07 16:53:35 +0100

    further unittests

--- python/main/test_brickbreaker_main.py
+++ python/main/test_brickbreaker_main.py
@@ -1,4 +1,10 @@
+import unittest
 from brickbreaker_main import *
 
-def test_placeholder():
-    assert 1 == 1
+class BrickbreakerTest(unittest.TestCase):
+
+    def test_amounts():
+        assert len(walls) == 4, "incorrect amount of walls"
+
+    def test_types():
+        assert type(all_blocks) == list, "incorrect type"
